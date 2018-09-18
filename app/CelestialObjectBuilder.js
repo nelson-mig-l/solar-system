99e8fbf129fbc3a23462a2462b0424f2e917f96f
nelson-mig-l <nelson.mig.l@gmail.com>
2018-09-18 23:14:56 +0200

    enable rings

--- app/CelestialObjectBuilder.js
+++ app/CelestialObjectBuilder.js
@@ -7,6 +7,9 @@ function CelestialObjectBuilder(materialLoader, name) {
     this.orbitingDistance = 0;
     this.orbitalSpeed = 0;
     this.axialSpeed = 0;
+    
+    this.ringRadius = 0;
+    this.ringWidth = 0;
 }
 
 CelestialObjectBuilder.prototype.constructor = CelestialObjectBuilder;
@@ -42,6 +45,12 @@ CelestialObjectBuilder.prototype.withOrbit = function (orbiting, distance) {
     return this;
 };
 
+CelestialObjectBuilder.prototype.withRing = function (radius, width) {
+    this.ringRadius = radius;
+    this.ringWidth = width;
+    return this;
+};
+
 CelestialObjectBuilder.prototype.build = function () {
     var material;
     if (this.light === undefined) {
@@ -51,13 +60,38 @@ CelestialObjectBuilder.prototype.build = function () {
     }
     var obj = new CelestialObject(this.size, material);
     obj.name = this.name;
+    
     if (this.light !== undefined) {
         obj.add(this.light);
     }
+    
     if (this.orbitingWhat !== undefined) {
         obj.orbiting(this.orbitingWhat, this.orbitingDistance);
     }
     obj.speed(this.orbitalSpeed, this.axialSpeed);
+    
+    if (this.ringRadius !== 0 && this.ringWidth !== 0) {
+        var ringMaterial = this.materialLoader.asLambertWithTransparency(this.name);
+        var ringGeometry = new THREE.RingBufferGeometry( this.ringRadius, this.ringRadius + this.ringWidth, 8, 1 );
+        var uvs = ringGeometry.attributes.uv.array;
+        var phi = ringGeometry.parameters.phiSegments; // 8
+        var theta = ringGeometry.parameters.thetaSegments; // 1
+        for ( var c = 0, j = 0; j <= phi; j ++ ) {
+            for ( var i = 0; i <= theta; i ++ ) {
+                uvs[c++] = i / theta,
+                uvs[c++] = j / phi;
+            }
+        }
+        var ringMesh = new THREE.Mesh( ringGeometry, ringMaterial );
+        ringMesh.position.x = obj.mesh.position.x;
+        ringMesh.position.y = obj.mesh.position.y;
+        ringMesh.position.z = obj.mesh.position.z;
+
+        ringMesh.rotation.x = Math.PI/2;
+        
+        obj.add(ringMesh);
+    }
+    
     return obj;
 };
 
